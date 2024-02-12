5b4a27f9906ad23739b992734a71a3da38b881e5
vorpality <138505432+vorpality@users.noreply.github.com>
2024-02-12 15:13:49 +0200

    autocomplete styling

--- src/blocks/claim-business/frontend.js
+++ src/blocks/claim-business/frontend.js
@@ -8,9 +8,31 @@ jQuery(document).ready(function($) {
     source: placesData.map(place => ({ label: place.name, value: place.id })),
     select: function(event, ui) {
       document.querySelector('#business-data').setAttribute('data-place-id', ui.item.value);
+    },
+    open: function() {
+      const inputWidth = $(businessSearchInput).outerWidth(); // Get the outer width of the input field
+      $('.ui-autocomplete').css('width', inputWidth + 'px'); // Set the width of the dropdown menu
     }
   });
+
+  $(window).resize(function() {
+    const businessSearchInput = document.querySelector('.business-search');
+
+    const inputWidth = $(businessSearchInput).outerWidth(); // Get the outer width of the input field
+    $('.ui-autocomplete').css('width', inputWidth + 'px'); // Set the width of the dropdown menu
+
+    // Get the position of the input field
+    const inputOffset = $(businessSearchInput).offset();
+    const inputHeight = $(businessSearchInput).outerHeight();
+
+    // Position the dropdown right below the input field
+    $('.ui-autocomplete').css({
+      top: inputOffset.top + inputHeight,
+      left: inputOffset.left
+    });
+  });
 });
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
