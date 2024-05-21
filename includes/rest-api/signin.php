8c2d51319d98dddce335a69689ee9fb42b1f5c7f
vorpality <138505432+vorpality@users.noreply.github.com>
2024-05-21 13:36:32 +0300

    rest api mobile

--- includes/rest-api/signin.php
+++ includes/rest-api/signin.php
@@ -10,18 +10,39 @@ function ept_uf_rest_api_signin_handler($request){
       empty($params['password'])
   )
   {
-      return $response;
+    $response['message'] = 'Please provide both username and password';
   }
 
   $email = sanitize_email($params['user_login']);
   $password = sanitize_text_field($params['password']);
-  
+  // Mobile login 
+
+  if (isset($params['app']) && $params['app'] == 'true') {
+    $username = get_user_by('email', $email)->user_login;
+    $user = wp_authenticate($username, $password);
+
+    if (is_wp_error($user)) {
+        return new WP_Error('invalid_credentials', 'Invalid username or password.', array('status' => 403));
+    }
+
+    wp_set_current_user($user->ID);
+    wp_set_auth_cookie($user->ID, true);
+
+    return rest_ensure_response(array(
+        'status' => 200,
+        'message' => 'Login successful', 
+        'user' => $user));
+}
+
+
+ // Normal login
   $user =  wp_signon([
       'user_login' => $email,
       'user_password' => $password,
       'remember' => true
   ]);
   
+
   if(is_wp_error($user)){
       return $user;
       return $response;
--- index.php
+++ index.php
@@ -38,5 +38,6 @@ add_filter('wp_mail_from_name', 'ept_uf_new_mail_from_name');
 add_action('login_form_resetpass', 'ept_pw_reset_redirect');
 add_action('login_form_rp','do_password_reset');
 add_action('login_form_resetpass', 'do_password_reset');
+//add_filter('rest_authentication_errors', 'rest_cookie_check_errors');
 add_action('init', 'ept_user_flow_load_php_translations');
 add_action('wp_enqueue_scripts', 'ept_user_flow_load_block_translations',100); 
